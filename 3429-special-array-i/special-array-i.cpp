b48c304f832432c697ac35e62368a2d901e87b2f
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-02-01 18:27:49 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (61.71%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        if(nums.size() == 1){
+            return true;
+        }
+        else{
+            for(int i =1; i<nums.size(); i++){
+                if((nums[i] & 1) == (nums[i-1] & 1) ){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
