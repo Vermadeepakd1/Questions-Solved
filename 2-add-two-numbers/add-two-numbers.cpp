dcde380ac38e10b0e44858759b4da142747ea764
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-05-06 10:12:24 +0530

    Time: 4 ms (21.83%) | Memory: 77.1 MB (43.23%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,40 +1,39 @@
 /**
- *Definition for singly-linked list.
- *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
- *};
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
-class Solution
-{
-    public:
-        ListNode* addTwoNumbers(ListNode *l1, ListNode *l2)
-        {
-            ListNode *dummy = new ListNode(0);
-            ListNode *curr = dummy;
-            int carry = 0;
-            while (l1 != nullptr || l2 != nullptr || carry > 0)
-            {
-                int sum = carry;
-                if (l1 != nullptr)
-                {
-                    sum += l1->val;
-                    l1 = l1->next;
-                }
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        ListNode *temp1 = l1, *temp2 = l2;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
 
-                if (l2 != nullptr)
-                {
-                    sum += l2->val;
-                    l2 = l2->next;
-                }
+        int carry = 0, sum = 0;
+        while (temp1 || temp2 || carry) {
+            int first = temp1 ? temp1->val : 0;
+            int second = temp2 ? temp2->val : 0;
 
-                carry = sum / 10;
-                curr->next = new ListNode(sum % 10);
-                curr = curr->next;
+            sum = first + second + carry;
+            int newdata = sum % 10;
+            carry = sum / 10;
+
+            temp->next = new ListNode(newdata);
+            temp = temp -> next;
+            if (temp1) {
+                temp1 = temp1->next;
             }
-            return dummy->next;
+            if (temp2) {
+                temp2 = temp2->next;
+            }
         }
+        return dummy->next;
+    }
 };
