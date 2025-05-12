352722e02006d2f0fe1273ff1b87949590ca3b32
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-05-12 15:37:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (43.23%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -17,12 +17,12 @@ public:
             words.push_back(temp);
             temp = "";
         }
-
-        for(int i = 0; i < words.size(); i++){
+        int n = words.size();
+        for(int i = 0; i < n; i++){
             if(i!=0){
                 temp += " ";
             }
-            temp += words[words.size() - i -1];
+            temp += words[n - i -1];
         }
         return temp;
     }
