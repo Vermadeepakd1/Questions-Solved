c589082571f3412ffa19bda9d720f2e1128bd7ca
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-05-16 15:21:53 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (6.19%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int target = (nums.size()/2)+1;
+        unordered_map<int,int> mp;
+        for(int num : nums){
+            mp[num]++;
+            if(mp[num] == target){
+                return num;
+            }
+        }
+        return -1;
+    }
+};
