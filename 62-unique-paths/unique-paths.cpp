3dd961f39a8125679569e1edd535362cf6cf3596
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-05-17 08:25:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (86.22%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,20 +1,13 @@
 class Solution {
-    int findcount(int currr, int currc, int m, int n, vector<vector<int>>&paths){
-        if(currr == m-1 && currc == n-1){
-            return 1;
-        }
-        if(currr >= m || currc >= n){
-            return 0;
-        }
-        if(paths[currr][currc] != -1){
-            return paths[currr][currc];
-        }
-        return paths[currr][currc] = findcount(currr,currc+1,m,n,paths) + findcount(currr+1,currc,m,n,paths);
-
-    }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>paths(m,vector<int>(n,-1));
-        return findcount(0,0,m,n,paths);
+        int N = m + n -2;
+        int r = min(m-1,n-1);
+        double res = 1;
+
+        for(int i = 1; i<= r; i++){
+            res = res * (N-r+i)/i;
+        }
+        return (int)res;
     }
 };
