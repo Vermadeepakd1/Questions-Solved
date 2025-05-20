5a1a3fada9514fdf9ea518285435334c28b99005
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-05-20 07:09:23 +0530

    Time: 152 ms (11.13%) | Memory: 325.4 MB (29.92%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -4,14 +4,12 @@ public:
         //when you see ranges like this question, use difference array
         int n = nums.size();
         vector<int> diffarray(n+1,0);
-        for(auto query : queries){
-            int l = query[0], r= query[1];
-            diffarray[l]++;
-            diffarray[r+1]--;
+        for(auto query : queries){ 
+            diffarray[query[0]]++;
+            diffarray[query[1] + 1]--;
         }
-        for(int i = 1; i<=n ; i++){
+        for(int i = 1; i<=n; i++){
             diffarray[i] += diffarray[i-1];
-            cout << diffarray[i] << " ";
         }
         for(int i = 0; i<n; i++){
             if(nums[i] > diffarray[i]){
