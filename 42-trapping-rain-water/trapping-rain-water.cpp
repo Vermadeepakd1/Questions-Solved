a381b296cc7f04edf6418a2c450e3b81ee406018
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-05-30 20:18:26 +0530

    Time: 0 ms (100.00%) | Memory: 26.8 MB (44.30%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int  n= height.size();
-        vector<int> pre(n),post(n);;
+        vector<int> post(n);;
         int premaxi = 0, postmaxi = 0;
         for(int i = 0; i<n; i++){
-            premaxi = max(premaxi,height[i]);
-            pre[i] = premaxi;
-
             postmaxi = max(postmaxi,height[n-i-1]);
             post[n-i-1] = postmaxi;
         }
 
         int result = 0;
         for(int i = 0; i<n; i++){
-            result += min(pre[i],post[i]) - height[i];
+            premaxi = max(premaxi,height[i]);
+            result += min(premaxi,post[i]) - height[i];
         }
         return result;
     }
