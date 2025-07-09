8846e1a60533e04e3b022839cad599adf85ddaf3
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-07-09 11:34:28 +0530

    Time: 0 ms (100.00%) | Memory: 119.9 MB (55.39%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,24 +1,26 @@
 class Solution {
-    long long solve(vector<vector<int>>& questions, int idx, vector<int> &dp) {
-        if (idx >= questions.size()) {
-            return 1LL * 0;
+    long long solve(vector<vector<int>>& questions, int idx, vector<long long>& dp) {
+        int n = questions.size();
+        dp[n - 1] = questions[n - 1][0];
+
+        for (int i = n - 2; i >= 0; i--) {
+            long long nextIndex = 1LL * i + questions[i][1] + 1;
+            long long solved =
+                questions[i][0] + ((nextIndex < n) ? dp[nextIndex] : 0);
+
+            long long skip = 1LL * dp[i + 1];
+
+            dp[i] = max(skip, solved);
         }
 
-        if(dp[idx] != -1) return dp[idx];
-
-        long long solved =
-            questions[idx][0] + solve(questions, idx + questions[idx][1] + 1,dp);
-        long long skip = 1LL * solve(questions, idx + 1,dp);
-
-        dp[idx] = max(skip,solved);
-        return max(skip, solved);
+        return dp[0];
     }
 
 public:
     long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
-        vector<int> dp(n+1, -1);
-        long long ans = solve(questions, 0,dp);
+        vector<long long> dp(n + 1, -1);
+        long long ans = solve(questions, 0, dp);
         return ans;
     }
 };
