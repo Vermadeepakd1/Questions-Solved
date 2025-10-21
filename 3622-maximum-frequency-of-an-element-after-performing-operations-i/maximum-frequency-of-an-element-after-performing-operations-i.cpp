70df1503ca038f74d872a8abc090e2247d90e023
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-10-21 22:36:30 +0530

    Time: 2 ms (99.51%) | Memory: 100.9 MB (76.21%) - LeetSync

---
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        int maxi = *max_element(nums.begin(),nums.end()) + k;
+
+        vector<int> freq(maxi+1,0);
+        for(int &i : nums){
+            freq[i]++;
+        }
+        vector<int> cf(maxi+1,0);
+        for(int i = 1; i<=maxi; i++){
+            cf[i] = freq[i] + cf[i-1];
+        }
+        int ans = -1;
+        for(int i = 0; i<=maxi; i++){
+            int l = max(i -k,0);
+            int r = min(maxi,i+k);
+
+            int total = cf[r] - (l>0 ? cf[l-1] :0);
+            int targetcount = cf[i] - (i>0 ? cf[i-1] : 0);
+
+            int needconvert = total - targetcount;
+
+            int maxpossible = targetcount + min(needconvert,numOperations);
+
+            ans = max(ans,maxpossible);
+        }
+        return ans;
+
+    }
+};
