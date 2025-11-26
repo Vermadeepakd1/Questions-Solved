780b68c348599822bd7cfe02c7491af68da92bbd
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-11-26 10:16:11 +0530

    Time: 19 ms (29.39%) | Memory: 14.2 MB (29.40%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -6,19 +6,13 @@ public:
         int right = 0, left = 0;
         int maxlen = 0;
         while (right < n) {
-            while (right < n && st.find(s[right]) == st.end()) {
-                st.insert(s[right]);
-                right++;
-            }
-
-            maxlen = max(maxlen, right - left);
-
-            while (s[left] != s[right]) {
+            while(st.find(s[right]) != st.end() ){
                 st.erase(s[left]);
                 left++;
             }
-            st.erase(s[left]);
-            left++;
+            st.insert(s[right]);
+            maxlen = max(maxlen, right - left + 1);
+            right++;
         }
         return maxlen;
     }
