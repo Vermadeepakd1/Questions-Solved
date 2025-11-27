a1ad0335b02b82adaec4d5a8d2d41d6f36c25306
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-11-27 21:33:03 +0530

    Time: 124 ms (78.70%) | Memory: 96.6 MB (79.59%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,24 +1,34 @@
 class Solution {
     int n;
     long long t[100001][2];
-    long long solve(vector<int>& nums, int idx, bool flag){
-        if(idx >= nums.size())return 0;
+    // long long solve(vector<int>& nums, int idx, bool flag){
+    //     if(idx >= nums.size())return 0;
 
-        if(t[idx][flag] != -1)return t[idx][flag];
+    //     if(t[idx][flag] != -1)return t[idx][flag];
         
-        long long skip = solve(nums,idx+1,flag);
-        int val = nums[idx];
-        if(!flag){
-            val = -val;
-        }
-        long long take = solve(nums, idx+1, !flag) + val;
+    //     long long skip = solve(nums,idx+1,flag);
+    //     int val = nums[idx];
+    //     if(!flag){
+    //         val = -val;
+    //     }
+    //     long long take = solve(nums, idx+1, !flag) + val;
 
-        return t[idx][flag] = max(skip,take);
-    }
+    //     return t[idx][flag] = max(skip,take);
+    // }
 public:
     long long maxAlternatingSum(vector<int>& nums) {
         int n = nums.size();
         memset(t, -1, sizeof(t));
-        return solve(nums, 0 , true);
+
+        t[0][0] = 0; // 0 =even
+        t[0][1] = nums[0];
+        for(int i =1; i<n; i++){
+            t[i][0] = max(t[i-1][0], t[i-1][1] - nums[i]);
+            t[i][1] = max(t[i-1][1], t[i-1][0] + nums[i] );
+        }
+
+        return max(t[n-1][0], t[n-1][1]);
+
+        // return solve(nums, 0 , true);
     }
 };
