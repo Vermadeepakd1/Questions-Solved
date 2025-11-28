1bc34a43eab5139d5d1b54f1d95b2840fe82fa9d
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-11-28 21:52:09 +0530

    Time: 80 ms (43.82%) | Memory: 14.3 MB (70.74%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,24 +1,37 @@
 class Solution {
-    int dp[2501][2501];
-    int solve(vector<int>& nums, int idx, int p){
-        if(idx >= nums.size())return 0;
-        if(p!= -1 && dp[idx][p] != -1)return dp[idx][p];
-        int take = 0, skip = 0;
-        //take
-        if(p == -1 || nums[idx] > nums[p]){
-            take = 1 + solve(nums,idx+1,idx);
-        }
-        //skip
-        skip = solve(nums,idx+1,p);
+    // int dp[2501][2501];
+    // int solve(vector<int>& nums, int idx, int p){
+    //     if(idx >= nums.size())return 0;
+    //     if(p!= -1 && dp[idx][p] != -1)return dp[idx][p];
+    //     int take = 0, skip = 0;
+    //     //take
+    //     if(p == -1 || nums[idx] > nums[p]){
+    //         take = 1 + solve(nums,idx+1,idx);
+    //     }
+    //     //skip
+    //     skip = solve(nums,idx+1,p);
 
-        if(p!= -1){
-            dp[idx][p] = max(take,skip);
-        }
-        return  max(take,skip);
-    }
+    //     if(p!= -1){
+    //         dp[idx][p] = max(take,skip);
+    //     }
+    //     return  max(take,skip);
+    // }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,0,-1);
+        // memset(dp,-1,sizeof(dp));
+        // return solve(nums,0,-1);
+
+        int n = nums.size();
+        vector<int> dp(n,1);
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<=i; j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+            }
+        }
+
+        return *max_element(dp.begin(),dp.end());
     }
 };
