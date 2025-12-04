e3bac2be629d8d766793e818decc31d045ae4fc8
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-12-04 20:31:00 +0530

    Time: 530 ms (14.02%) | Memory: 8.7 MB (64.28%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    bool check(string& s, int i, int j) {
+        if (i > j)
+            return true;
+        if (s[i] == s[j])
+            return check(s, i + 1, j - 1);
+        return false;
+    }
+
+public:
+    int countSubstrings(string s) {
+        int n = s.length();
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i ; j < n; j++) {
+                if (check(s, i, j))
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+};
