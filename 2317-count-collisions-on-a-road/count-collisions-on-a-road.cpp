27299388764d266d2e7a68b1a6f97aa44a2f4eb8
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-12-04 14:53:29 +0530

    Time: 3 ms (95.07%) | Memory: 19.2 MB (64.06%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
@@ -1,42 +1,15 @@
 class Solution {
 public:
     int countCollisions(string directions) {
-        int ans = 0;
-        stack<char> st;
-        for (char i : directions) {
-            if (i == 'R')
-                st.push(i);
-            else if (i == 'S') {
+        int n = directions.size();
+        int ans = 0, l =0, r = n-1;
+        while(l < n && directions[l] == 'L')l++;
+        while(r >=0 && directions[r] == 'R')r--;
 
-                while (!st.empty()) {
-                    if (st.top() == 'R') {
 
-                        ans += 1;
-                    }
-                    st.pop();
-                }
-
-                st.push('S');
-            } else {
-                if (st.empty()) {
-                    continue;
-                } else if (st.top() == 'R') {
-                    st.pop();
-                    ans += 2;
-                    while (!st.empty()) {
-                    if (st.top() == 'R') {
-
-                        ans += 1;
-                    }
-                    st.pop();
-                }
-                    st.push('S');
-                } else if (st.top() == 'S') {
-                    ans += 1;
-                }
-            }
+        for(int i = l ; i<=r; i++){
+            if(directions[i] != 'S')ans+=1;
         }
-        
         return ans;
     }
 };
