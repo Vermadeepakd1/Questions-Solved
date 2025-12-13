dcbd8471a90cddb74d2b572c10eb93dc308200fd
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2025-12-13 21:48:50 +0530

    Time: 67 ms (48.65%) | Memory: 156.1 MB (46.55%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
@@ -1,54 +1,41 @@
 class Solution {
-    int n;
-    vector<vector<int>> t;
-    int child1(vector<vector<int>>& fruits){
-        int ans = 0;
-        for(int i = 0; i<n; i++){
-            ans += fruits[i][i];
-        }
-        return ans;
-    }
-
-    int child2(int i, int j, vector<vector<int>>& fruits){
-        if(i >= n || j<0 || j>=n)return 0;
-
-        if(i==n-1 && j== n-1)return 0;
-
-        if(i >=j)return 0;
-
-        if(t[i][j] != -1)return t[i][j];
-
-        int down = fruits[i][j] + child2(i+1,j,fruits);
-        int right = fruits[i][j] + child2(i+1,j+1,fruits);
-        int left = fruits[i][j] + child2(i+1,j-1,fruits);
-
-        return t[i][j] = max(down,max(left,right));
-
-    }
-
-    int child3(int i, int j, vector<vector<int>>& fruits){
-        if(i >= n || j<0 || i< 0)return 0;
-
-        if(i==n-1 && j== n-1)return 0;
-
-        if(i <= j)return 0;
-
-        if(t[i][j] != -1)return t[i][j];
-
-        int up = fruits[i][j] + child3(i-1,j+1,fruits);
-        int right = fruits[i][j] + child3(i,j+1,fruits);
-        int down = fruits[i][j] + child3(i+1,j+1,fruits);
-
-        return t[i][j] = max(down,max(up,right));
-    }
 public:
     int maxCollectedFruits(vector<vector<int>>& fruits) {
-        n = fruits.size();
-        t.resize(n,vector<int>(n,-1));
-        int c1 = child1(fruits);
-        int c2 = child2(0,n-1,fruits);
-        int c3 = child3(n-1,0,fruits);
+        int n = fruits.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        return c1+c2+c3;
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result += fruits[i][i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i < j && i + j < n - 1) {
+                    dp[i][j] = 0;
+                } else if (i > j && i + j < n - 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = fruits[i][j];
+                }
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] += max({dp[i - 1][j - 1], dp[i - 1][j],
+                                 (j + 1 < n) ? dp[i - 1][j + 1] : 0});
+            }
+        }
+
+        for (int j = 1; j < n; j++) {
+            for (int i = j + 1; i < n; i++) {
+                dp[i][j] += max({dp[i][j - 1], dp[i - 1][j - 1],
+                                 (i + 1 < n) ? dp[i + 1][j - 1] : 0});
+            }
+        }
+
+        return result + dp[n - 2][n - 1] + dp[n - 1][n - 2];
     }
 };
