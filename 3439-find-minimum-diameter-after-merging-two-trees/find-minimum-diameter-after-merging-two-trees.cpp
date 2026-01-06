c419d704d3264018cd02a7b26bf0e2f512558078
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-01-06 23:18:03 +0530

    Time: 908 ms (19.07%) | Memory: 430.7 MB (21.22%) - LeetSync

--- 3439-find-minimum-diameter-after-merging-two-trees/find-minimum-diameter-after-merging-two-trees.cpp
+++ 3439-find-minimum-diameter-after-merging-two-trees/find-minimum-diameter-after-merging-two-trees.cpp
@@ -1,23 +1,25 @@
 class Solution {
-    pair<int,int> bfs(unordered_map<int,vector<int>>&adj, int src){
+    pair<int, int> bfs(unordered_map<int, vector<int>>& adj, int src) {
         queue<int> q;
         q.push(src);
 
-        unordered_map<int,bool>visited;
+        unordered_map<int, bool> visited;
         visited[src] = true;
 
-        int lastnode = src , distance = -1;
+        int lastnode = src, distance = -1;
 
-        while(!q.empty()){
+        while (!q.empty()) {
             int x = q.size();
             distance++;
             lastnode = q.front();
-            for(int i = 0; i<x; i++){
+            for (int i = 0; i < x; i++) {
                 int nd = q.front();
                 q.pop();
-                visited[nd] = true;
-                for(auto nbr: adj[nd]){
-                    if(!visited[nbr])q.push(nbr);
+                for (auto nbr : adj[nd]) {
+                    if (!visited[nbr]) {
+                        visited[nbr] = true;
+                        q.push(nbr);
+                    }
                 }
             }
         }
@@ -25,17 +27,19 @@ class Solution {
         return {lastnode, distance};
     }
 
-    int findDiameter(unordered_map<int,vector<int>>&adj){
-        auto [farthest, dist] = bfs(adj,0);
-        auto [other, diameter] = bfs(adj,farthest);
+    int findDiameter(unordered_map<int, vector<int>>& adj) {
+        auto [farthest, dist] = bfs(adj, 0);
+        auto [other, diameter] = bfs(adj, farthest);
 
         return diameter;
     }
-public:
-    int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
-        unordered_map<int,vector<int>> adj1, adj2;
 
-        for(auto &vec : edges1){
+public:
+    int minimumDiameterAfterMerge(vector<vector<int>>& edges1,
+                                  vector<vector<int>>& edges2) {
+        unordered_map<int, vector<int>> adj1, adj2;
+
+        for (auto& vec : edges1) {
             int u = vec[0];
             int v = vec[1];
 
@@ -43,7 +47,7 @@ public:
             adj1[v].push_back(u);
         }
 
-        for(auto &vec : edges2){
+        for (auto& vec : edges2) {
             int u = vec[0];
             int v = vec[1];
 
@@ -54,9 +58,9 @@ public:
         int d1 = findDiameter(adj1);
         int d2 = findDiameter(adj2);
 
-        int newmax = (d1+1)/2 + (d2+1)/2 + 1;
+        int newmax = (d1 + 1) / 2 + (d2 + 1) / 2 + 1;
 
-        int ans = max({d1,d2,newmax});
+        int ans = max({d1, d2, newmax});
 
         return ans;
     }
