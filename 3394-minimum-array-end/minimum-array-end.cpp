98ca15fa32700eafe4902b8a17e78ef6b7454bcd
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-01-15 14:52:11 +0530

    Time: 1752 ms (9.15%) | Memory: 8.6 MB (84.31%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -2,18 +2,9 @@ class Solution {
 public:
     long long minEnd(int n, int x) {
         long long start = x;
-        long long num = n-1;
-        long long mask = 1;
 
-        while(num != 0){
-            while((mask & start) != 0)mask= (mask << 1);
-
-            if(num&1){
-                start |= mask;
-
-            }
-            num = (num >> 1);
-            mask = (mask << 1);
+        for(int i=0; i<n-1; i++){
+           start = (x |(start+1)) ;
         }
         return start;
     }
