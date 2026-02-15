1d459b1d61d8e1da067bfc71318f6727c0561d78
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-02-15 09:58:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (22.87%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int i = a.size() - 1, j = b.size() - 1;
         int carry = 0;
+        int n1 = a.length(), n2 = b.length();
+
         string result = "";
+        int x=n1-1, y = n2-1;
+        while(x>=0 || y>=0){
+            int num1 = (x>=0 ? (a[x]-'0'):0);
+            int num2 = (y>=0 ? (b[y]-'0'):0);
+            int sum = num1+num2+carry;
 
-        while (i >= 0 || j >= 0 || carry) {
-            int bitA = i >= 0 ? a[i--] - '0' : 0;
-            int bitB = j >= 0 ? b[j--] - '0' : 0;
-
-            int sum = bitA + bitB + carry;
-            result += (sum % 2) + '0';
-            carry = sum / 2;
+            if(sum <=1){
+                result += to_string(sum);
+                carry=0;
+            }else if(sum==2){
+                carry = 1;
+                result += '0';
+            }else{
+                carry=1;
+                result+='1';
+            }
+            x--,y--;
         }
-
-        reverse(result.begin(), result.end());
+        if(carry >0)result +='1';
+        reverse(result.begin(),result.end());
         return result;
     }
 };
