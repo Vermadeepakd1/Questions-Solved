45b2f9ca9e69890a2c3eb4a7622214d32ffa5886
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-07-28 22:25:03 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (42.84%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -6,26 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-    int max_d = 0;
-    int maxDepth(TreeNode* root) {
-        if (root == NULL)
-            return 0;
+    int diameter = 0;
+    int solve(TreeNode* root){
+        if(root == NULL)return 0;
+        if(root->left == NULL && root->right == NULL)return 1;
 
-        int leftH = maxDepth(root->left);
-        int rightH = maxDepth(root->right);
-        max_d = max(max_d,leftH + rightH  );
-        return max(leftH, rightH) + 1;
+        int left = solve(root->left) ;
+        int right = solve(root->right);
+        diameter = max(diameter,left+right);
+
+        return 1+ max(left,right);
     }
-
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        int ans = 0;
-        maxDepth(root);
-        return max_d;
+        solve(root);
+        return diameter;
     }
 };
