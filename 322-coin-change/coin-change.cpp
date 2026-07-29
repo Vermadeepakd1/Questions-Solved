f5d5b1f3bd79cad43180109466295c5c3ec39ecd
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-07-29 18:45:12 +0530

    Time: 20 ms (74.80%) | Memory: 13.5 MB (97.98%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,20 +1,28 @@
 class Solution {
-public:
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
+    int dp[10001];
+    int findcoins(vector<int>& coins, int amount){
+        if(amount ==0)return 0;
 
-        vector<int> dp(amount + 1, INT_MAX);
-        dp[0] = 0;
+        if(dp[amount] != -1)return dp[amount];
 
-        for (int i = 1; i <= amount; i++) {
-            for (int j : coins) {
-                if (i - j >= 0 && dp[i - j] != INT_MAX) {
-                    dp[i] = min(dp[i], dp[i - j] + 1);
-                }
+        int mincoins = INT_MAX;
+        for(int i : coins){
+            if(amount - i >= 0){
+                int rem = findcoins(coins, amount - i);
+                if(rem != INT_MAX)
+                mincoins =  min(1+rem,mincoins);
             }
         }
-        if (dp[amount] != INT_MAX)
-            return dp[amount];
-        return -1;
+        return dp[amount] = mincoins;
+    }
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        memset(dp,-1,sizeof(dp));
+
+        int n = coins.size();
+
+        int result =  findcoins(coins,amount);
+        if(result == INT_MAX)return -1;
+        return result;
     }
 };
