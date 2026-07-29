61914855cba0124a645bb6959c53f4396a4e576a
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-07-30 00:31:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (26.15%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int numDecodings(string s) {
+        if (s.empty() || s[0] == '0') return 0;
+
         int n = s.length();
-        vector<int> dp(n + 1, 0);
-        if (s[0] == '0')
-            return 0;
+        vector<int> dp(n + 1, 0); 
+        
         dp[0] = 1;
         dp[1] = 1;
+
         for (int i = 2; i <= n; i++) {
-            int oneDigit = s[i - 1] - '0';
-            if (oneDigit >= 1 && oneDigit <= 9) {
-                dp[i] += dp[i - 1];
+            if (s[i-1] != '0') {
+                dp[i] += dp[i-1];
             }
 
-            int twoDigits = stoi(s.substr(i - 2, 2));
-            if (twoDigits >= 10 && twoDigits <= 26) {
-                dp[i] += dp[i - 2];
+            int two_digit = stoi(s.substr(i-2, 2));
+            if (two_digit >= 10 && two_digit <= 26) {
+                dp[i] += dp[i-2];
             }
         }
+
         return dp[n];
     }
 };
