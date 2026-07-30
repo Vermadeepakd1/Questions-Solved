43d7ae160870a9d9f40dd43b48d2c2cfc5a18173
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-07-30 11:50:48 +0530

    Time: 22 ms (15.91%) | Memory: 22.1 MB (5.44%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size(), m = matrix[0].size();
-        unordered_set<int> rowset, colset;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    rowset.insert(i);
-                    colset.insert(j);
+        vector<int> rows,cols;
+
+        int m = matrix.size(), n = matrix[0].size();
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(matrix[i][j] ==0){
+                    rows.push_back(i);
+                    cols.push_back(j);
                 }
             }
         }
-        for (int x : rowset) {
-            for (int i = 0; i < m; i++) {
-                matrix[x][i] = 0;
+
+        for(int i : cols){
+            for(int j = 0;  j < m; j++){
+                matrix[j][i] = 0;
             }
         }
-        for (int y : colset) {
-            for (int i = 0; i < n; i++) {
-                matrix[i][y] = 0;
+
+        for(int i : rows){
+            for(int j = 0; j<n; j++){
+                matrix[i][j] = 0;
             }
         }
+
     }
 };
