378cb1e60dc5fcac30eda903fdda5483a7a8f1d5
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-07-31 15:44:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (63.30%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -8,31 +8,30 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode(-1);
+        ListNode *dummy = new ListNode(-1);
         ListNode* temp = dummy;
-        while (list1 != NULL && list2 != NULL) {
-            if (list1->val < list2->val) {
-                temp->next = list1;
-                list1 = list1->next;
-                temp = temp->next;
-            } else {
-                temp->next = list2;
-                list2 = list2->next;
-                temp = temp->next;
+
+        ListNode* f = list1, *s = list2;
+
+        while(f != NULL && s != NULL){
+            if(f->val <= s->val){
+                temp->next = f;
+                f=f->next;
+            }else{
+                temp->next=s;
+                s=s->next;
             }
-        }
-        while (list1 != NULL) {
-            temp->next = list1;
-            list1 = list1->next;
             temp = temp->next;
         }
-        while (list2 != NULL) {
-            temp->next = list2;
-            list2 = list2->next;
-            temp = temp->next;
+        if (f != NULL) {
+            temp->next = f;
+        } else {
+            temp->next = s;
         }
         return dummy->next;
     }
