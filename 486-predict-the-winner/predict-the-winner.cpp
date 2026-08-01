15484d1454ffe16949b6394394d5d31c54024536
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-08-01 16:13:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (45.39%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,25 +1,25 @@
 class Solution {
-    bool checkwin(vector<int>& nums, int aturn, int csum1, int csum2, int start,
-                  int end) {
-        if (start > end) {
-            if (csum1 >= csum2)
-                return true;
-            return false;
-        }
-
-        if (aturn) {
-            return checkwin(nums, !aturn, csum1 + nums[start], csum2, start + 1, end) ||
-                   checkwin(nums, !aturn, csum1 + nums[end], csum2, start, end - 1);
-        } else {
-            return checkwin(nums, !aturn, csum1, csum2 + nums[start], start + 1, end) &&
-                   checkwin(nums, !aturn, csum1, csum2 + nums[end], start, end - 1);
-        }
-    }
-
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        return checkwin(nums, true, 0, 0, 0,
-                        n - 1); // nums, Aturn , currsum1, currsum2, start, end
+        
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+        
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1; 
+                
+                int takeLeft = nums[i] - dp[i + 1][j];
+                int takeRight = nums[j] - dp[i][j - 1];
+                
+                dp[i][j] = max(takeLeft, takeRight);
+            }
+        }
+        
+        return dp[0][n - 1] >= 0;
     }
 };
