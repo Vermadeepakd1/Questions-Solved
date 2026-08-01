6966e1b99cd97050ef94e5b418731714c4ceeb3e
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-08-01 16:59:14 +0530

    Time: 3 ms (72.70%) | Memory: 26.9 MB (99.68%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,38 +10,21 @@
  * };
  */
 class Solution {
+    TreeNode *build(int &idx, vector<int>& preorder, vector<int>& inorder, int s, int e){
+        if(s > e)return NULL;
+            TreeNode *temp = new TreeNode(preorder[idx]);
+        
+        int iidx = find(inorder.begin(),inorder.end(),preorder[idx])-inorder.begin();
+        idx++;
+        temp -> left = build(idx, preorder, inorder,s,iidx-1);
+        temp->right = build(idx, preorder, inorder , iidx+1,e);
+
+        return temp;
+    }
 public:
-int find(int n, vector<int> &in, int size){
-        for(int i = 0; i<size; i++){
-            if(in[i] == n){
-                return i;
-            }
-        }
-        return -1;
-    }
-    TreeNode* solve(vector<int> &in, vector<int> &pre, int &index, int start, int end , int n, map<int,int> &mp){
-        
-        if(index >= n || start > end){
-            return NULL;
-        }
-        
-        int element = pre[index++];
-        int p = mp[element];
-        
-        TreeNode *root = new TreeNode(element);
-        root->left = solve(in,pre,index,start,p-1,n,mp);
-        root->right = solve(in,pre,index,p+1,end,n,mp);
-        
-        return root;
-    }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        map<int,int> mp;
-        for(int i = 0; i<inorder.size(); i++){
-            mp[inorder[i]] = i;
-        }
-        int preorderindex = 0;
-        int n = inorder.size();
-        TreeNode * result = solve(inorder,preorder,preorderindex,0,n-1,n,mp);
-        return result;
+        int idx = 0;
+        int n = preorder.size();
+        return build(idx, preorder, inorder, 0, n-1 ); 
     }
 };
