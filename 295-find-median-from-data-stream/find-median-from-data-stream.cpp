2c1ff0292c901340446d6d2fac3f298df5695354
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-08-02 15:57:59 +0530

    Time: 98 ms (21.99%) | Memory: 148.6 MB (30.14%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -3,8 +3,7 @@ public:
     priority_queue<int> maxheap;
     priority_queue<int,vector<int>,greater<int>> minheap;
 
-    MedianFinder() {
-        
+    MedianFinder() {    
     }
     
     void addNum(int num) {
@@ -12,17 +11,15 @@ public:
         minheap.push(maxheap.top());
         maxheap.pop();
 
-        if(minheap.size() > maxheap.size()){
+        while(minheap.size() > maxheap.size()){
             maxheap.push(minheap.top());
             minheap.pop();
         }
     }
     
     double findMedian() {
-        if(maxheap.size() > minheap.size()){
-            return maxheap.top();
-        }
-        return (double)(maxheap.top() + minheap.top())/2;
+        if(maxheap.size() > minheap.size())return maxheap.top();
+        else return (double)(minheap.top()+maxheap.top() )/ 2;
     }
 };
 
