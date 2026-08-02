2f8840c1b0186e8d267455e1ff7b177c35750445
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-08-02 16:45:00 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (7.63%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,20 +1,16 @@
 class Solution {
-    int robhouse(vector<int>& nums,int s, int e){
-        int prev1 = 0,prev2 = 0;
-        prev1 = nums[s];
-        for(int i = s+ 1; i <=e; i++){
-            int curr = max(prev1, prev2 + nums[i]);
-            prev2 = prev1;
-            prev1 = curr;
+    int solve(vector<int>& nums, int start){
+        int n = nums.size()-1;
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        for(int i =1; i<=n; i++){
+            dp[i][1] = dp[i-1][0] + nums[i+start-1];
+            dp[i][0] = max(dp[i-1][0],dp[i-1][1]);
         }
-
-        return prev1;
+        return max(dp[n][0],dp[n][1]);
     }
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1)return nums[0];
-
-        return max(robhouse(nums,0,n-2), robhouse(nums,1,n-1));
+        if (nums.size() == 1) return nums[0];
+        return max(solve(nums,0) ,solve(nums,1));
     }
 };
