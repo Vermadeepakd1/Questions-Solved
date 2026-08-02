2bec9e8eb1b81d7007d9b13252069c96f1ec3503
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-08-02 20:18:09 +0530

    Time: 43 ms (83.73%) | Memory: 12.1 MB (33.34%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -2,31 +2,34 @@ class Solution {
 public:
     int minCut(string s) {
         int n = s.length();
-        vector<vector<bool>> dp(n,vector<bool>(n,false));
-        for(int l = 0; l<n; l++){
-            for(int i = 0; i<n-l; i++){
-                int j = i+l;
-                if(l == 0)dp[i][j] = true;
-                else if(s[i] == s[j]){
-                    if(l == 1)dp[i][j] = true;
-                    else dp[i][j] = dp[i+1][j-1];
+        if (n <= 1) return 0;
+        
+        vector<vector<bool>> isPal(n, vector<bool>(n, false));
+        for(int g = 0; g < n; g++){
+            for(int i = 0; i < n - g; i++){
+                int j = i + g;
+                if(s[i] == s[j] && (g <= 2 || isPal[i + 1][j - 1])){
+                    isPal[i][j] = true;
                 }
             }
         }
-
-        vector<int> ans(n,0);
-        for(int i = 0; i<n; i++){
-            if(dp[0][i])ans[i] = 0;
-            else{
-                ans[i] = INT_MAX;
-                for(int k = 0; k<i; k++){
-                    if(dp[k+1][i] && 1 + ans[k] < ans[i]){
-                        ans[i] = 1 + ans[k];
+        
+        vector<int> dp(n, 0);
+        
+        for(int i = 0; i < n; i++){
+            if(isPal[0][i]) {
+                dp[i] = 0; 
+            } else {
+                dp[i] = i; 
+                
+                for(int j = 1; j <= i; j++){
+                    if(isPal[j][i]){
+                        dp[i] = min(dp[i], dp[j - 1] + 1);
                     }
                 }
             }
         }
-
-        return ans[n-1];
+        
+        return dp[n - 1];
     }
 };
