a9f7059770686dd142065a3cb9ee5f2e3981cf76
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-08-24 09:51:25 +0530

    Time: 112 ms (41.29%) | Memory: 87.1 MB (96.05%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+        
+        int sum = 0;
+        for (int stone : stones) {
+            sum += stone;
+        }
+
+        int ans = sum;
+
+        for (int i = n - 2; i >= 1; i--) {
+            sum -= stones[i + 1]; 
+            ans = max(ans, sum - ans);
+        }
+
+        return ans;
+    }
+};
