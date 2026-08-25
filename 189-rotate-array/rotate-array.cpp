ffc50eb58bd3ff27508c7aaf6cc189903ff5cca6
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-08-25 07:25:03 +0530

    Time: 0 ms (100.00%) | Memory: 263.5 MB (7.65%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k%n;
+        ::rotate(nums.begin(),nums.end()-k, nums.end());
+    }
+};
