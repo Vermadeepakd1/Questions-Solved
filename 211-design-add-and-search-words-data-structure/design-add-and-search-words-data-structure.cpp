2c673cc5a91923ad1f43cba42fb6ce97c4fbaf3b
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-09-22 21:16:51 +0530

    Time: 393 ms (52.13%) | Memory: 590.8 MB (37.65%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,61 @@
+class WordDictionary {
+    struct trieNode{
+        trieNode * children[26];
+        bool isEndOfWord;
+    };
+    trieNode* getnode(){
+        trieNode* newnode = new trieNode();
+        newnode->isEndOfWord = false;
+        for(int i = 0; i<26; i++){
+            newnode->children[i] = NULL;
+        }
+        return newnode;
+    }
+    trieNode* root;
+public:
+    WordDictionary() {
+        root= getnode();
+    }
+    
+    void addWord(string word) {
+        trieNode *crawler = root;
+        for(int i = 0; i<word.length(); i++){
+            int index= word[i]-'a';
+            if(!crawler->children[index])crawler->children[index] = getnode();
+            crawler = crawler->children[index];
+        }
+
+        crawler->isEndOfWord = true;
+    }
+
+    bool searchUtil(trieNode* root, string word)
+    {
+        trieNode *crawler = root;
+        for(int i = 0; i<word.length(); i++){
+            char ch = word[i];
+            if(ch == '.'){
+                for(int j = 0; j<26; j++){
+                    if(crawler->children[j] != NULL){
+                        if(searchUtil(crawler->children[j],word.substr(i+1)))return true;
+                    }
+                }
+                return false;
+            }
+            else if(crawler->children[ch-'a']==NULL){
+                return false;
+            }
+            crawler = crawler->children[ch-'a'];
+        }
+        return (crawler !=NULL && crawler->isEndOfWord == true);
+    }
+    bool search(string word) {
+        return searchUtil(root,word);
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
