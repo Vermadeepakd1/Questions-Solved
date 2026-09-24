10762cbce997fc6075318b39afc0f07e0e8125af
Deepak Verma <147736162+Vermadeepakd1@users.noreply.github.com>
2026-09-24 10:10:35 +0530

    Time: 0 ms (100.00%) | Memory: 31 MB (52.36%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int digsum(int n){
         int sum = 0;
-        while(n!= 0){
+        while(n!=0){
             sum += (n%10);
             n/=10;
         }
         return sum;
     }
-
     int smallestIndex(vector<int>& nums) {
-        for(int i = 0; i<nums.size(); i++){
+        int n = nums.size();
+        for(int i=0; i<n; i++){
             if(digsum(nums[i])==i)return i;
         }
         return -1;
